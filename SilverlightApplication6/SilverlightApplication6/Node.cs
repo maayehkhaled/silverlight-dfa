c372fd91686c3620c80aa519dcb63659cc804188
phucluoi@gmail.com <phucluoi@gmail.com@6a7c3e8a-d668-293f-e2e7-39ee89091f06>
2011-06-30 12:20:57 +0000

    remove enume

--- SilverlightApplication6/SilverlightApplication6/Node.cs
+++ SilverlightApplication6/SilverlightApplication6/Node.cs
@@ -5,12 +5,25 @@ using System.Text;
 
 namespace SilverlightApplication6
 {
+    /** 
+    * doc: Real programmers don't comment their code.  It was hard to write, it
+      should be hard to understand and even harder to modify.
+    */
     class Node
     {
         public String nodeLabel;
-        public List<Tuple> adjacent = new List<Tuple>();
+        public List<Tuple> adjacent ;
         double x;
         double y;
         bool isEnd;
+        
+        public Node(String label, double x, double y, bool isEnd)
+        {
+            this.nodeLabel = label;
+            this.x = x;
+            this.y = y;
+            this.isEnd = isEnd;
+            this.adjacent = new List<Tuple> ();
+        }
     }
 }
