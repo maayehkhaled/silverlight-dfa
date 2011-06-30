fcb84f783fea97faa6205a8a92fc9fc2f8535fc5
phucluoi@gmail.com <phucluoi@gmail.com@6a7c3e8a-d668-293f-e2e7-39ee89091f06>
2011-06-30 11:21:41 +0000

    basis Klassen

---
+++ trunk/SilverlightApplication6/SilverlightApplication6/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilverlightApplication6
+{
+    class Node
+    {
+        public String nodeLabel;
+        public List<Tuple> adjacent = new List<Tuple>();
+        double x;
+        double y;
+        bool isEnd;
+    }
+}
