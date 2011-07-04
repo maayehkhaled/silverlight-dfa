75038d1947b595e5114f1832007895c469060ca0
phucluoi@gmail.com <phucluoi@gmail.com@6a7c3e8a-d668-293f-e2e7-39ee89091f06>
2011-07-04 00:12:15 +0000

    zwischen bk

--- trunk/SilverlightApplication6/SilverlightApplication6/EPoint.cs
+++ trunk/SilverlightApplication6/SilverlightApplication6/EPoint.cs
@@ -30,17 +30,23 @@ namespace SilverlightApplication6
 		 * along the vector v
 		 */
 		{
-			this.x = this.x - v.x;
-			this.y = this.y - v.y;
+			x = x - v.x;
+			y = y - v.y;
 		}
 
 		public void rotateCoordinate(EVector v1, EVector v2)
 		/* compute the the coordinate in new rotated coordination system.
 		 * New Coordinate system is the result of rotating the coordinate system 
 		 * of a angle phi, cos(phi) = (v1 * v2) / abs(a) * abs(b)
+		 * if the orientation of v1 and v2 positiv (uhrzeigersinn) the rotation angel
+		 * is positive, else negativ.
 		 */
 		{
 			var phi = Math.Acos( v1 * v2 / (v1.abs() * v2.abs()) );
+			if (v1.orientationWith(v2) < 0)
+			{
+				phi = -phi;
+			}
 			rotateCoordinate(phi);
 		}
 
@@ -51,6 +57,15 @@ namespace SilverlightApplication6
 			newY = -x * Math.Sin(phi) + y * Math.Cos(phi);
 			x = newX; y = newY;
 		}
+
+		public double distanceTo(EPoint p)
+		{
+			return Math.Sqrt( (x - p.x)*(x-p.x) + (y-p.y)*(y-p.y) );
+		}
+		public Point makeSPoint()
+		{
+			return new Point(x, -y);
+		}
 	}
 
 	public class EVector
@@ -59,30 +74,60 @@ namespace SilverlightApplication6
 		public double y = 0;
 		public EVector(EPoint begin, EPoint end)
 		{
-			this.x = end.x - begin.x;
-			this.y = end.y - begin.y;
+			x = end.x - begin.x;
+			y = end.y - begin.y;
 		}
 		public EVector(EPoint free)
 		{
-			this.x = free.x;
-			this.x = free.y;
+			x = free.x;
+			y = free.y;
 		}
 		public EVector()
 		{
-			this.x = 0.0; this.y = 0.0;
+			x = 0.0; 
+			y = 0.0;
+		}
+		public EVector(double x, double y)
+		{
+			this.x = x; this.y = y;
 		}
 
 		public static double operator *(EVector v, EVector u)
 		/*compute scale production of v und u*/
 		{
-			double X = v.x * u.x;
-			double Y = v.y * u.y;
-			return Math.Sqrt(X * X + Y * Y);
+			return v.scaleProd(u);
 		}
 
+		public double scaleProd(EVector v)
+		/* compute scale production of v and this */
+		{
+			double X = x * v.x;
+			double Y = y * v.y;
+			return X + Y;
+		}
+
+		public double orientationWith(EVector v)
+		/* compute the "cross production" this [x] v, assume all z's coordiante equals 0 */
+		{
+			return x * v.y - y * v.x;
+		}
+
+
+		public double angleWith(EVector v)
+		/* return the cos of the angle between vector and v */
+		{
+			return  (this * v ) / (abs() *v.abs()) ;
+		}
 		public double abs()
 		{
 			return Math.Sqrt(x * x + y * y);
+		}
+
+		public static EVector operator -(EVector v)
+		{
+			v.x = -v.x;
+			v.y = -v.y;
+			return v;
 		}
 	}
 }
--- trunk/SilverlightApplication6/SilverlightApplication6/GraphDrawer.cs
+++ trunk/SilverlightApplication6/SilverlightApplication6/GraphDrawer.cs
@@ -20,6 +20,7 @@ namespace SilverlightApplication6
         /*constructor get a list of nodes*/
         public List<Node> nodes;
 		public Canvas dfaDiplay;
+		static int i = 0;
 
         public GraphDrawer(List<Node> nodes, Canvas c)
         {
@@ -39,7 +40,7 @@ namespace SilverlightApplication6
             }
         }
 
-		public void drawEdges(Canvas c)
+		public void drawEdges()
 		/* draw edges on the Canvas c */
 		{
 			foreach (Node i in nodes)
@@ -47,14 +48,46 @@ namespace SilverlightApplication6
 				List<Tuple<Node,string>> adjacent = i.adjacent;
 				foreach ( Tuple<Node,string> j in adjacent)
 				{
-					Tuple<Point, Point, Point, Point> coordinate = computeEdge(i, j.Item1);
+					/*Tuple<Point, Point, Point, Point> coordinate = computeEdge(i, j.Item1);
 					constructBezier(coordinate);
+					 */
 				}
 			}
 		}
 
 		private void constructBezier(Tuple<Point, Point, Point, Point> coordinate)
 		{
+			
+			var bz = new BezierSegment();
+			//var p1 = new Point(100, 30);
+			//bz.Point1 = p1;
+			bz.Point1 = coordinate.Item2;
+			//var p2 = new Point(200, 30);
+			//bz.Point2 = p2;
+			bz.Point2 = coordinate.Item3;
+			var p3 = new Point(300, -40);
+			bz.Point3 = p3;
+			//bz.Point3 = coordinate.Item4;
+
+			var pc = new PathSegmentCollection();
+			pc.Add(bz);
+
+			var pf = new PathFigure();
+			pf.StartPoint = new Point(0, 0);
+			//pf.StartPoint = coordinate.Item1;
+			pf.Segments = pc;
+			var pfc = new PathFigureCollection();
+			pfc.Add(pf);
+			var pg = new PathGeometry();
+			pg.Figures = pfc;
+
+			var path = new Path();
+			path.Data = pg;
+			path.Stroke = new SolidColorBrush(Colors.Black);
+
+			dfaDiplay.Children.Add(path);
+			path.SetValue(Canvas.TopProperty,i+0.0);
+			i += 10;
 			
 		}
 
@@ -65,18 +98,130 @@ namespace SilverlightApplication6
 			i.presentor.SetValue(Canvas.LeftProperty, i.x);
 		}
 
-		public Tuple<Point, Point, Point, Point> computeEdge(Node i, Node j)
+		//public Tuple<Point, Point, Point, Point> computeEdge(Node i, Node j)
+		public Tuple<EPoint, EPoint, EPoint, EPoint> computeEdge(Node i, Node j, TextBox log)
 		/* compute 4 point to draw  */
 		{
-			Point beginPoint = new Point();
-			Point endPoint = new Point();
-			Point m1Point = new Point();
-			Point m2Point = new Point();
+			Point beginPoint;
+			Point endPoint;
+			Point m1Point;
+			Point m2Point;
 
-			/*EPoint begin = new EPoint(i.x, -i.y);
-			begin.transformCoordinate(
+			EPoint begin = new EPoint(i.x, -i.y);
+			
+			log.Text += "begin: x" + begin.x + "\n";
+			log.Text += "     : y" + begin.y + "\n";
+			
+
+			EPoint end = new EPoint(j.x, -j.y);
+			/*
+			log.Text += "end: x" + end.x + "\n";
+			log.Text += "   : y" + end.y + "\n";
 			*/
-			return new Tuple<Point, Point, Point, Point>(beginPoint, m1Point, m2Point, endPoint);
+
+			/* move the coordinate base to begin */
+			EVector transform = new EVector(i.x + i.presentor.RenderSize.Width/2 , 
+				- (i.y + i.presentor.RenderSize.Height/2));
+			/*
+			log.Text += "transform vector: x " + transform.x + "\n";
+			log.Text += "                : y " + transform.y + "\n";
+			*/
+			begin.transformCoordinate(transform);
+			
+
+			
+			log.Text += "after transform:\n";
+			log.Text += "begin: x " + begin.x + "\n";
+			log.Text += "     : y " + begin.y + "\n";
+			
+
+			end.transformCoordinate(transform);
+			/*
+			log.Text += "end: x" + end.x + "\n";
+			log.Text += "   : y" + end.y + "\n";
+			*/
+
+			/* rotate the coordinate, so that the vector <begin->end> is the x-axes */
+			EVector v1 = new EVector(1, 0);
+			EVector v2 = new EVector(begin, end);
+			
+			begin.rotateCoordinate(v1, v2);
+			
+			
+			log.Text += "after rotation:\n";
+			log.Text += "begin: x" + begin.x + "\n";
+			log.Text += "     : y" + begin.y + "\n";
+			
+			
+			end.rotateCoordinate(v1, v2);
+			
+			/*
+			log.Text += "end: x" + end.x + "\n";
+			log.Text += "   : y" + end.y + "\n";
+			*/
+
+			/* compute the points */
+			EPoint p1;
+			EPoint p2;
+			EPoint p3;
+			EPoint p4;
+			double alpha;
+			var distance = begin.distanceTo(end);
+			/*TODO: calibiert Fehlertolranz*/
+			if (end.x > 0)
+			{
+				alpha = Math.PI / 6;
+				log.Text += "alpha: " + alpha + "\n";
+				
+				p1 = new EPoint(i.presentor.RenderSize.Width * Math.Cos(alpha),
+					i.presentor.RenderSize.Width * Math.Sin(alpha));
+				
+				p2 = new EPoint(distance / 3,
+					distance / 3);
+				p3 = new EPoint(2 * distance / 3,
+					distance / 3);
+				p4 = new EPoint(distance - j.presentor.RenderSize.Width * Math.Cos(alpha),
+					j.presentor.RenderSize.Width * Math.Sin(alpha));
+			}else
+			{
+				alpha = Math.PI  + Math.PI / 6;
+				p1 = new EPoint(i.presentor.RenderSize.Width * Math.Cos(alpha),
+					i.presentor.RenderSize.Width * Math.Sin(alpha));
+				p2 = new EPoint(- distance / 3,
+					- distance / 3);
+				p3 = new EPoint(- 2 * distance / 3,
+					- distance / 3);
+				p4 = new EPoint(- distance - j.presentor.RenderSize.Width * Math.Cos(alpha),
+					j.presentor.RenderSize.Width * Math.Sin(alpha));
+			}
+			/* transform backward */
+			log.Text += "before bw transform:\n";
+			log.Text += "p1: x " + p1.x + "\n";
+			log.Text += "  : y " + p2.x + "\n";
+
+			p1.rotateCoordinate(v2, v1);
+			log.Text += "after bw rotation:\n";
+			log.Text += "p1: x " + p1.x + "\n";
+			log.Text += "  : y " + p2.x + "\n";
+
+			p1.transformCoordinate(-transform);
+			p2.rotateCoordinate(v2, v1);
+			p2.transformCoordinate(-transform);
+			p3.rotateCoordinate(v2, v1);
+			p3.transformCoordinate(-transform);
+			p4.rotateCoordinate(v2, v1);
+			p4.transformCoordinate(-transform);
+			
+			/* create Silverlight Point  */
+			beginPoint = p1.makeSPoint();
+			m1Point = p2.makeSPoint();
+			m2Point = p3.makeSPoint();
+			endPoint = p4.makeSPoint();
+			log.Text += "result:\n";
+			log.Text += "begin point: x " + beginPoint.X + "\n";
+			log.Text += "           : y " + beginPoint.Y + "\n";
+			return new Tuple<EPoint, EPoint, EPoint, EPoint>(p1, p2, p3, p4);
+			//return new Tuple<Point, Point, Point, Point>(beginPoint, m1Point, m2Point, endPoint);
 		}
     }
 }
